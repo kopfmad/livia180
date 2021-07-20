40e4eb5305a805eb234638cb28776f45e6b15777
kopfmad <santana.agr@gmail.com>
2021-07-20 19:21:48 -0300

    Iniciando o repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import livia1 from './images/livia1.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App2">
+      <img src={livia1} alt="livia 1" />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempora,
+      soluta, ad deserunt rem omnis harum sit similique voluptatibus quae
+      consectetur quaerat voluptatem earum cupiditate modi, temporibus culpa
+      velit laborum qui. Asperiores quibusdam maiores accusantium aperiam
+      excepturi, provident suscipit odit doloribus nostrum reprehenderit
+      assumenda sequi quae quaerat iusto at quis eos nulla eveniet qui eius
+      eaque sunt. Doloremque repellendus maiores in. Quis id perspiciatis ipsa
+      provident hic corporis assumenda numquam? Eveniet, aliquam deserunt ipsam
+      eaque nesciunt nulla recusandae fugiat fuga optio assumenda nemo facilis,
+      officia autem accusantium laudantium commodi reprehenderit reiciendis.
+      Earum, delectus autem. Quos, laudantium minima assumenda, possimus, eaque
+      nesciunt iure dolore nulla culpa odit in dolor aut provident? Facere optio
+      aliquam voluptas dignissimos ab ratione suscipit, voluptatum minima dolore
+      ducimus sint perspiciatis soluta blanditiis nesciunt repudiandae nihil
+      praesentium fuga. Autem aspernatur vel reprehenderit culpa officia eaque
+      dolores molestias? Assumenda laborum praesentium impedit unde suscipit eum
+      vel officia est temporibus? Quas dolorum accusamus labore amet vitae
+      necessitatibus hic nesciunt, distinctio mollitia! Tenetur possimus
+      quaerat, ratione nobis qui impedit nisi dignissimos error rem vitae?
+      Reprehenderit eos commodi accusamus ipsa, reiciendis ut, hic quia quam
+      sint soluta inventore vel earum in quis magnam! Amet atque rerum sapiente
+      officia, repellat nulla perferendis quibusdam, placeat minus voluptates
+      consequatur laboriosam ipsum aliquam. Velit veritatis itaque possimus
+      numquam expedita dolores incidunt culpa quidem commodi ipsum officiis
+      odit, accusamus dignissimos vero, voluptates error odio veniam ratione
+      similique ducimus. Iste, libero! Sint dignissimos fugiat aliquid,
+      similique ut, voluptatem iusto aut voluptatibus tempora fugit unde dolore
+      sit! Porro, libero eos? Voluptas blanditiis obcaecati magnam nisi
+      voluptatibus neque aliquam voluptatem impedit voluptates expedita placeat
+      labore sint, tempore inventore suscipit sit, officiis molestiae, delectus
+      nulla excepturi similique. Expedita quia maiores reiciendis ipsam incidunt
+      temporibus deleniti, suscipit, soluta itaque reprehenderit qui. Est
+      perferendis iusto, nostrum eius ducimus consectetur. Laudantium asperiores
+      quia modi ullam repudiandae voluptatibus vel animi numquam vitae mollitia,
+      quae totam labore tempora cupiditate delectus ab repellendus eum eveniet
+      quasi ipsam nemo magnam quidem minima. Perferendis ipsam provident nemo
+      quo placeat repellendus quas animi dolorum aliquam? Velit, aperiam
+      suscipit totam perferendis illum error vitae. Ratione quidem, eaque
+      laudantium rem doloribus quis sit repellat tempore quo, cum repudiandae
+      quod harum dignissimos consequuntur sequi tenetur laboriosam similique.
+      Ratione fugit at nostrum quibusdam animi vero sint repudiandae vel, illum
+      omnis eos facere ab reiciendis deleniti facilis, aspernatur tempora
+      placeat deserunt quo praesentium itaque cum. Laudantium, hic. Aut beatae
+      dolorem illum assumenda accusantium fugit ratione blanditiis esse facilis
+      officia nesciunt non tempore rem quas eaque a quae repudiandae,
+      perferendis itaque ea perspiciatis ullam? Aut, tempore cumque veniam quod
+      ratione quidem tenetur vitae? Ex beatae minus soluta natus perferendis et,
+      pariatur quia fuga mollitia non id aspernatur ad, ab hic accusantium cum
+      ea iste obcaecati vitae nesciunt consequatur doloribus atque. Aliquid ex
+      veniam, soluta asperiores fugit repellendus. Dignissimos nihil dolores vel
+      officiis consequuntur hic veniam, tenetur ut nesciunt non nobis illo
+      tempore natus sunt aliquam praesentium ullam dicta corrupti! Autem minima
+      non ipsa consequatur repellendus dolor voluptatem rerum molestias dicta
+      ducimus itaque, illum est iusto.
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
