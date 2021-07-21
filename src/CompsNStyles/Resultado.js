7c2e72b2d089a106fac89f66c8c573e9d133e92c
kopfmad <santana.agr@gmail.com>
2021-07-21 09:50:08 -0300

    teminei o css

--- src/CompsNStyles/Resultado.js
+++ src/CompsNStyles/Resultado.js
@@ -3,5 +3,10 @@ import React, { useState } from 'react'
 export default function Resultado() {
   let [finalresult, setFinalresult] = useState('20')
 
-  return <div className="Resultado"> O resultado é {finalresult}</div>
+  return (
+    <div className="Resultado">
+      {' '}
+      O resultado é <b>{finalresult}</b>
+    </div>
+  )
 }
