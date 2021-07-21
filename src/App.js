39a06e5b98c3a4794823ed1cb3d7b16315cbeb4d
kopfmad <santana.agr@gmail.com>
2021-07-21 09:30:11 -0300

    coloquei todos os itens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './index.css'
-import livia1 from './images/livia1.png'
+import MyInput from './CompsNStyles/MyInput'
+import Resultado from './CompsNStyles/Resultado'
 
 function App() {
   return (
     <div className="App">
       <div className="livia_container"></div>
+      <MyInput placeholder="Digite o primeiro número"></MyInput>
+      <MyInput placeholder="Digite o segundo número"></MyInput>
+      <Resultado></Resultado>
     </div>
   )
 }
---
+++ src/CompsNStyles/Resultado.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+export default function Resultado() {
+  let [finalresult, setFinalresult] = useState('20')
+
+  return <div className="Resultado"> O resultado é {finalresult}</div>
+}
